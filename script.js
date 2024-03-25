201123f3486f1a23faed9fca917778bf6168c0e1
Larisa Moșuț <larisa.mosut@emanuel.ro>
2024-03-25 17:05:11 +0200

    am adaugat soarele

--- script.js
+++ script.js
@@ -20,19 +20,29 @@ function draw() {
     fill("brown")
     rect(600, 160, 50, 100);
 
-    
     //window
     fill("lightblue")
     rect(600, 350, 60, 85);
     rect(540, 350, 60, 85);
 
-        //roof window
-        circle(500, 230, 60);
-        stroke("black")
-        line(500, 200, 500, 260);
-        line(470, 230, 530, 230);
+    //roof window
+    circle(500, 230, 60);
+    stroke("black")
+    line(500, 200, 500, 260);
+    line(470, 230, 530, 230);
 
+    //sun
+    stroke("black")
+    fill("yellow");
+    circle(100, 100, 80);
 
-
+    fill("yellow");
+    for (let i = 0; i < 360; i += 45) {
+        push();
+        translate(100, 100);
+        rotate(radians(i));
+        triangle(-10, -50, 0, -90, 10, -50);
+        pop();
+    }
 
 }
